bd3282c6d687dd21f43ca8c87c04f65dfc98603e
BernardoHalfeld <nebiasbernardo@gmail.com>
2022-03-06 17:28:50 -0300

    11

--- Lixeira.c
+++ Lixeira.c
@@ -1,4 +1,5 @@
 #include <Servo.h>   //Biblioteca do Servo Motor
+
 Servo servo;     
 int trigPin = 2;    
 int echoPin = 3;   
@@ -17,33 +18,33 @@ void setup() {
     servo.detach(); 
 } 
 
-void measure() {  
- //digitalWrite(HIGH);
-digitalWrite(trigPin, LOW);
-delayMicroseconds(5);
-digitalWrite(trigPin, HIGH);
-delayMicroseconds(15);
-digitalWrite(trigPin, LOW);
-pinMode(echoPin, INPUT);
-duration = pulseIn(echoPin, HIGH);
-dist = (duration/2) / 29.1;//Obtem a distância
+void measure() {  //digitalWrite(HIGH);
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(5);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(15);
+    digitalWrite(trigPin, LOW);
+    pinMode(echoPin, INPUT);
+    duration = pulseIn(echoPin, HIGH);
+    dist = (duration/2) / 29.1;//Obtem a distância
 }
-void loop() { 
-  for (int i=0;i<=2;i++) {//verifica a distância
-    measure();               
-   aver[i]=dist;            
-    delay(10);//adiciona um atraso as medições
-  }
- dist=(aver[0]+aver[1]+aver[2])/3;    
 
-if ( dist<50 ) {
- servo.attach(servoPin);
-  delay(1);
- servo.write(0);  
- delay(3000);       
- servo.write(150);    
- delay(1000);
- servo.detach();      
-}
-Serial.print(dist);
+void loop() { 
+    for (int i=0;i<=2;i++) {//verifica a distância
+        measure();               
+        aver[i]=dist;            
+        delay(10);//adiciona um atraso as medições
+    }
+    dist=(aver[0]+aver[1]+aver[2])/3;    
+
+    if ( dist<50 ) {
+        servo.attach(servoPin);
+        delay(1);
+        servo.write(0);  
+        delay(3000);       
+        servo.write(150);    
+        delay(1000);
+        servo.detach();      
+    }
+    Serial.print(dist);
 }
